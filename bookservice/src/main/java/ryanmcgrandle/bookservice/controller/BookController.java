62d179f57b930c3edb3575b0067e74b8abc5c78f
Syyren <129496054+Syyren@users.noreply.github.com>
2024-03-28 14:04:48 -0600

    added exception handling and swagger annotations

--- bookservice/src/main/java/ryanmcgrandle/bookservice/controller/BookController.java
+++ bookservice/src/main/java/ryanmcgrandle/bookservice/controller/BookController.java
@@ -1,7 +1,10 @@
 package ryanmcgrandle.bookservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import ryanmcgrandle.bookservice.model.Book;
+import ryanmcgrandle.bookservice.model.Quantity;
 import ryanmcgrandle.bookservice.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -25,54 +28,116 @@ public class BookController
 
     //inserts a new achievement into the "db" via the service
     @PostMapping("/create")
-    public ResponseEntity<Book> createBook(@RequestBody Book book)
+    @Operation(summary = "Makes a new Book.")
+    public ResponseEntity<Object> createBook(@RequestBody Book book)
     {
-        bookService.saveBook(book);
-        return ResponseEntity.ok(book);
+        try
+        {
+            bookService.saveBook(book);
+            return ResponseEntity.ok(book);
+        }
+        catch (RuntimeException e)
+        {
+            String errorMessage = "Failed to create the book: " + e.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        }
     }
     //gets all books in the db from the service
     @GetMapping("/get/all")
-    public List<Book> getAllBooks()
+    @Operation(summary = "Gets a list of Books from the Database.")
+    public ResponseEntity<List<Book>> getAllBooks()
     {
-        return bookService.getAllBooks();
+        List <Book> booklist = bookService.getAllBooks();
+        if (booklist.isEmpty())
+        {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(booklist);
+        }
+        return ResponseEntity.ok(booklist);
     }
-    //endpoint that gets an achievement from the "db" via its id from the service
+    //endpoint that gets a book from the db via its id from the service
     @GetMapping("/get/{bookId}")
-    public ResponseEntity<Book> getBookById(@PathVariable Long bookId)
+    @Operation(summary = "Gets a Book from the Database via its id")
+    public ResponseEntity<Object> getBookById(@PathVariable Long bookId)
     {
-        Book book = bookService.getById(bookId);
-        return ResponseEntity.ok(book);
+        try
+        {
+            Book book = bookService.getById(bookId);
+            return ResponseEntity.ok(book);
+        }
+        catch (RuntimeException e)
+        {
+            String errorMessage = e.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        }
     }
     //endpoint that updates a book that already exists in the db by id through the service
     @PutMapping("/update/{bookId}")
-    public ResponseEntity<Book> updateBook(@PathVariable Long bookId, @RequestBody Book updatedBook)
+    @Operation(summary = "Updates a Book in the Database via its id")
+    public ResponseEntity<Object> updateBook(@PathVariable Long bookId, @RequestBody Book updatedBook)
     {
-        Book book = bookService.getById(bookId);
-        updatedBook.setBookId(book.getBookId());
-        bookService.updateBook(updatedBook);
-        return ResponseEntity.ok(updatedBook);
+        try
+        {
+            Book book = bookService.getById(bookId);
+            updatedBook.setBookId(book.getBookId());
+            bookService.updateBook(updatedBook);
+            return ResponseEntity.ok(updatedBook);
+        }
+        catch (RuntimeException e)
+        {
+            String errorMessage = e.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        }
     }
     //endpoints that update a books quantity via its id in the db
-    @PutMapping("/restock/{bookId}")
-    public ResponseEntity<Book> restockBook(@PathVariable Long bookId, @RequestBody int qty)
+    @PatchMapping("/restock/{bookId}")
+    @Operation(summary = "Specifically handles adding to a Book's qty via its id.")
+    public ResponseEntity<Object> restockBook(@PathVariable Long bookId, @RequestBody Quantity amount)
     {
-        Book book = bookService.getById(bookId);
-        bookService.stockBook(book, qty);
-        return ResponseEntity.ok(book);
+        try
+        {
+            int qty = amount.getQty();
+            Book book = bookService.getById(bookId);
+            bookService.stockBook(book, qty);
+            return ResponseEntity.ok(book);
+        }
+        catch (RuntimeException e)
+        {
+            String errorMessage = e.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        }
     }
-    @PutMapping("/sell/{bookId}")
-    public ResponseEntity<Book> sellBook(@PathVariable Long bookId, @RequestBody int qty)
+    @PatchMapping("/sell/{bookId}")
+    @Operation(summary = "Specifically handles subtracting from a Book's qty via its id.")
+    public ResponseEntity<Object> sellBook(@PathVariable Long bookId, @RequestBody Quantity amount)
     {
-        Book book = bookService.getById(bookId);
-        bookService.sellBook(book, qty);
-        return ResponseEntity.ok(book);
+        try
+        {
+            int qty = amount.getQty();
+            Book book = bookService.getById(bookId);
+            bookService.sellBook(book, qty);
+            return ResponseEntity.ok(book);
+        }
+        catch (RuntimeException e)
+        {
+            String errorMessage = e.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        }
     }
     //endpoint that deletes an achievement from the "db" by id via the service
     @DeleteMapping("/delete/{bookId}")
+    @Operation(summary = "Deletes a Book from the Database via its id.")
     public ResponseEntity<String> deleteBook(@PathVariable Long bookId)
     {
-        Book book = bookService.getById(bookId);
-        bookService.deleteBook(book);
-        return ResponseEntity.ok("Deleted Successfully!");
+        try
+        {
+            Book book = bookService.getById(bookId);
+            bookService.deleteBook(book);
+            return ResponseEntity.ok("Deleted Successfully!");
+        }
+        catch (RuntimeException e)
+        {
+            String errorMessage = e.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        }
     }
 }
---
+++ bookservice/src/main/java/ryanmcgrandle/bookservice/model/Quantity.java
@@ -0,0 +1,14 @@
+package ryanmcgrandle.bookservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//Making a Java class to handle buying and selling through the request body.
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Quantity
+{
+    private int qty;
+}
